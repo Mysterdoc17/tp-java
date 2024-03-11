4f93f1c9243ebf7977799de9f769e4eee7d2e8b2
Mysterdoc17 <jeremie.baudon.17@gmail.com>
2024-03-11 14:44:17 +0100

    création design appli + ajout fichier modèle

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,23 +4,45 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.motion.utils.Oscillator.TAG
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     private val TAG = MainActivity::class.java.simpleName
+    val MyViewMode by viewModels<MyViewModel>()
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var nb = remember {
+                mutableStateOf(0)
+            }
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -32,6 +54,37 @@ class MainActivity : ComponentActivity() {
                             androidx.compose.material3.Text(text = "submit")
                         }
                     }
+                }
+                MaterialTheme {
+                    Scaffold(
+                        topBar = {
+                                 TopAppBar(
+                                     title = { Text(text = "My Application")},
+                                     modifier = Modifier.shadow(elevation = 10.dp)
+                                     )
+                        },
+                        content = {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(it)
+                            ) {
+                                Column(
+                                    verticalArrangement = Arrangement.SpaceEvenly,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier.fillMaxSize()
+                                ) {
+                                    Text(text = nb.value.toString(), fontSize = 50.sp)
+                                    Button(onClick = {
+                                        nb.value += 1
+                                    }) {
+                                        Text(text = "+")
+                                    }
+                                }
+                            }
+                        },
+                        bottomBar = {/**/}
+                    )
                 }
             }
         }
@@ -61,10 +114,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+    ) {
+    }
 }
 
 @Preview(showBackground = true)
